74662d6fb24656f52a4af90cf2102408b47d1930
RT-DETR-IMPROVE <renzhihao23@163.com>
2024-03-26 11:12:13 +0800

    message

--- 2dataStructure/2-2-0Stacks.cpp
+++ 2dataStructure/2-2-0Stacks.cpp
@@ -1,8 +1,46 @@
-// Implementation of stacks     // use Linked list
+// Linked list's Implementation of stacks     // use Linked list  //头插法
 #include<iostream>
 using namespace std;
+struct Node
+{
+    int data;
+    Node* link;
+};
+Node* top = NULL;
 
+void Push(int x)
+{
+     Node *temp = new Node(); 
+     temp->data = x;
+     temp->link = top;   //temp->link存放的是节点下一个的地址  节点的地址是  *temp = Ox123342..
+     top = temp;
+}
+ 
+void Pop()
+{
+    Node* temp;
+    if(top == NULL)return;
+    temp = top;
+    top = top->link;
+    delete(temp);
+
+}
+
+void Print()
+{
+    Node *temp = top;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->link;
+    }
+    cout << endl;
+}
 int main(void)
 {
+    Push(2);Print();
+    Push(3);Print();
+    Push(4);Print();
+    Pop();Print();
     return 0;
 }
